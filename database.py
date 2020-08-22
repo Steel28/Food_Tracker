3b284c90f48d357f5bdbf42f096d86df5d7b7c71
Steel28 <yassel.perez.19@gmail.com>
2020-08-21 21:53:56 -0500

    first commit

---
+++ database.py
@@ -0,0 +1,12 @@
+import sqlite3
+from flask import g
+
+def connect_db():
+    sql = sqlite3.connect('D:/Estudio/Front-end/The Ultimate Flask Course/The Ultimate Flask Course/FoodTracker/food_log.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
